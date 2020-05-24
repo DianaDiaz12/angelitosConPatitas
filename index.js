43cb7f4ac8eb2d1b1a668d1031cccce87486888f
DianaDiaz12 <ddiazmarti2@uniminuto.edu.co>
2020-05-24 15:45:55 -0500

    v.0.1.0 Se realiza consulta por nombre de producto

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const bodyparser = require('body-parser')
 
-var connect = mysql.createConnection({
+app.use(bodyparser.json());
+var mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'acp'
 });
+app.listen(3000, () => console.log('Express server is runnig at port no : 3000'));
 
-connect.connect(function (error) {
+mysqlConnection.connect((error) => {
     if (!!error) {
         console.log('Error');
     } else {
@@ -17,17 +20,62 @@ connect.connect(function (error) {
     }
 });
 
-app.get('/', function (req, resp) {
-
-    connect.query('SELECT * FROM products', function (error, rows, fields) {
-        if (!!error) {
-            console.log('Error');
-        } else {
-            console.log('Trae datos')
-            console.log(rows);
-        }
-    }
-    );
+//Get all employees
+app.get('/products', (req, res) => {
+    mysqlConnection.query('SELECT * FROM products', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
 });
-app.listen(1405);
+
+//Get an employees
+app.get('/products/:nombreProducto', (req, res) => {
+    mysqlConnection.query('SELECT * FROM products WHERE nombreProducto LIKE ? ', '%'+[req.params.nombreProducto]+'%', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+//Delete an employees
+app.delete('/products/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM products WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Deleted successfully.');
+        else
+            console.log(err);
+    })
+});
+
+//Insert an employees
+/*app.post('/products', (req, res) => {
+    let emp = req.body;
+    var sql = "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
+    CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
+    mysqlConnection.query(sql, [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary], (err, rows, fields) => {
+        if (!err)
+            rows.forEach(element => {
+                if(element.constructor == Array)
+                res.send('Inserted employee id : '+element[0].EmpID);
+            });
+        else
+            console.log(err);
+    })
+});
+
+//Update an employees
+app.put('/employees', (req, res) => {
+    let emp = req.body;
+    var sql = "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
+    CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
+    mysqlConnection.query(sql, [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary], (err, rows, fields) => {
+        if (!err)
+            res.send('Updated successfully');
+        else
+            console.log(err);
+    })
+});*/
 
